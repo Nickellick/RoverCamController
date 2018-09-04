d9854643c402866beffd465bcc16b8659401a146
Николай Поваров <kolya27.961@gmail.com>
2018-09-04 11:45:33 +1000

    First commit

---
+++ RoverCamController/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace RoverCamController
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.Up1 = new System.Windows.Forms.Button();
+            this.Right1 = new System.Windows.Forms.Button();
+            this.Down1 = new System.Windows.Forms.Button();
+            this.Left1 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.Up2 = new System.Windows.Forms.Button();
+            this.Up3 = new System.Windows.Forms.Button();
+            this.Down2 = new System.Windows.Forms.Button();
+            this.Down3 = new System.Windows.Forms.Button();
+            this.Left2 = new System.Windows.Forms.Button();
+            this.Left3 = new System.Windows.Forms.Button();
+            this.Right2 = new System.Windows.Forms.Button();
+            this.Right3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Up1
+            // 
+            this.Up1.Location = new System.Drawing.Point(160, 86);
+            this.Up1.Name = "Up1";
+            this.Up1.Size = new System.Drawing.Size(70, 35);
+            this.Up1.TabIndex = 0;
+            this.Up1.Text = "10";
+            this.Up1.UseVisualStyleBackColor = true;
+            this.Up1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Right1
+            // 
+            this.Right1.Location = new System.Drawing.Point(119, 127);
+            this.Right1.Name = "Right1";
+            this.Right1.Size = new System.Drawing.Size(35, 70);
+            this.Right1.TabIndex = 1;
+            this.Right1.Text = "10";
+            this.Right1.UseVisualStyleBackColor = true;
+            this.Right1.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Down1
+            // 
+            this.Down1.Location = new System.Drawing.Point(160, 203);
+            this.Down1.Name = "Down1";
+            this.Down1.Size = new System.Drawing.Size(70, 35);
+            this.Down1.TabIndex = 2;
+            this.Down1.Text = "10";
+            this.Down1.UseVisualStyleBackColor = true;
+            this.Down1.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Left1
+            // 
+            this.Left1.Location = new System.Drawing.Point(236, 127);
+            this.Left1.Name = "Left1";
+            this.Left1.Size = new System.Drawing.Size(35, 70);
+            this.Left1.TabIndex = 3;
+            this.Left1.Text = "10";
+            this.Left1.UseVisualStyleBackColor = true;
+            this.Left1.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(160, 127);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(70, 70);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "C";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(49, 328);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(201, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(256, 326);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(116, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Connect/Disconnect";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Up2
+            // 
+            this.Up2.Location = new System.Drawing.Point(160, 45);
+            this.Up2.Name = "Up2";
+            this.Up2.Size = new System.Drawing.Size(70, 35);
+            this.Up2.TabIndex = 7;
+            this.Up2.Text = "50";
+            this.Up2.UseVisualStyleBackColor = true;
+            this.Up2.Click += new System.EventHandler(this.Up2_Click);
+            // 
+            // Up3
+            // 
+            this.Up3.Location = new System.Drawing.Point(160, 4);
+            this.Up3.Name = "Up3";
+            this.Up3.Size = new System.Drawing.Size(70, 35);
+            this.Up3.TabIndex = 8;
+            this.Up3.Text = "100";
+            this.Up3.UseVisualStyleBackColor = true;
+            this.Up3.Click += new System.EventHandler(this.Up3_Click);
+            // 
+            // Down2
+            // 
+            this.Down2.Location = new System.Drawing.Point(160, 244);
+            this.Down2.Name = "Down2";
+            this.Down2.Size = new System.Drawing.Size(70, 35);
+            this.Down2.TabIndex = 9;
+            this.Down2.Text = "50";
+            this.Down2.UseVisualStyleBackColor = true;
+            this.Down2.Click += new System.EventHandler(this.Down2_Click);
+            // 
+            // Down3
+            // 
+            this.Down3.Location = new System.Drawing.Point(160, 285);
+            this.Down3.Name = "Down3";
+            this.Down3.Size = new System.Drawing.Size(70, 35);
+            this.Down3.TabIndex = 10;
+            this.Down3.Text = "100";
+            this.Down3.UseVisualStyleBackColor = true;
+            this.Down3.Click += new System.EventHandler(this.Down3_Click);
+            // 
+            // Left2
+            // 
+            this.Left2.Location = new System.Drawing.Point(277, 127);
+            this.Left2.Name = "Left2";
+            this.Left2.Size = new System.Drawing.Size(35, 70);
+            this.Left2.TabIndex = 11;
+            this.Left2.Text = "50";
+            this.Left2.UseVisualStyleBackColor = true;
+            this.Left2.Click += new System.EventHandler(this.Left2_Click);
+            // 
+            // Left3
+            // 
+            this.Left3.Location = new System.Drawing.Point(318, 127);
+            this.Left3.Name = "Left3";
+            this.Left3.Size = new System.Drawing.Size(35, 70);
+            this.Left3.TabIndex = 12;
+            this.Left3.Text = "100";
+            this.Left3.UseVisualStyleBackColor = true;
+            this.Left3.Click += new System.EventHandler(this.Left3_Click);
+            // 
+            // Right2
+            // 
+            this.Right2.Location = new System.Drawing.Point(78, 127);
+            this.Right2.Name = "Right2";
+            this.Right2.Size = new System.Drawing.Size(35, 70);
+            this.Right2.TabIndex = 13;
+            this.Right2.Text = "50";
+            this.Right2.UseVisualStyleBackColor = true;
+            this.Right2.Click += new System.EventHandler(this.Right2_Click);
+            // 
+            // Right3
+            // 
+            this.Right3.Location = new System.Drawing.Point(37, 127);
+            this.Right3.Name = "Right3";
+            this.Right3.Size = new System.Drawing.Size(35, 70);
+            this.Right3.TabIndex = 14;
+            this.Right3.Text = "100";
+            this.Right3.UseVisualStyleBackColor = true;
+            this.Right3.Click += new System.EventHandler(this.Right3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 331);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Status";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Right3);
+            this.Controls.Add(this.Right2);
+            this.Controls.Add(this.Left3);
+            this.Controls.Add(this.Left2);
+            this.Controls.Add(this.Down3);
+            this.Controls.Add(this.Down2);
+            this.Controls.Add(this.Up3);
+            this.Controls.Add(this.Up2);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.Left1);
+            this.Controls.Add(this.Down1);
+            this.Controls.Add(this.Right1);
+            this.Controls.Add(this.Up1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.RightToLeftLayout = true;
+            this.Text = "Rover Camera Control";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.Button Up1;
+        private System.Windows.Forms.Button Right1;
+        private System.Windows.Forms.Button Down1;
+        private System.Windows.Forms.Button Left1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button Up2;
+        private System.Windows.Forms.Button Up3;
+        private System.Windows.Forms.Button Down2;
+        private System.Windows.Forms.Button Down3;
+        private System.Windows.Forms.Button Left2;
+        private System.Windows.Forms.Button Left3;
+        private System.Windows.Forms.Button Right2;
+        private System.Windows.Forms.Button Right3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ RoverCamController/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace RoverCamController
+{
+    public partial class Form1 : Form
+    {
+        public static string chc = "chc";
+        public static int port = 8082;
+        public static string address = "192.168.0.61";
+        public static IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+        public static Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        public static bool conn_stat = false;
+
+        public void StatusText (string text)
+        {
+            textBox1.Clear();
+            textBox1.Paste(text);
+        }
+
+        public void send_data(string str)
+        {
+            try
+            {
+                byte[] data = Encoding.ASCII.GetBytes(str);
+                try
+                {
+                    socket.Send(data);
+                }
+                catch (System.ObjectDisposedException)
+                {
+                    StatusText("Server is disconnected. Try to connect again");
+                    conn_stat = false;
+                }
+            }
+            catch (System.Net.Sockets.SocketException)
+            {
+                StatusText("Server was disconnected\r\n");
+                conn_stat = false;
+            }
+        }
+
+        public string recv_data()
+        {
+            try
+            {
+                byte[] data = new byte[1024];
+                StringBuilder builder = new StringBuilder();
+                int bytes = 0;
+
+                do
+                {
+                    bytes = socket.Receive(data, data.Length, 0);
+                    builder.Append(Encoding.ASCII.GetString(data, 0, bytes));
+                }
+                while (socket.Available > 0);
+                return builder.ToString();
+            }
+            catch (System.ObjectDisposedException)
+            {
+                StatusText("Server is disconnected. Try to connect again");
+                conn_stat = false;
+                return "";
+            }
+        }
+
+        public void connect()
+        {
+            try
+            {
+                socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                socket.Connect(ipPoint);
+                StatusText("Connected to " + address + ":" + port + "\r\n");
+                conn_stat = true;
+            }
+            catch (System.Net.Sockets.SocketException)
+            {
+                StatusText("Server not found\r\n");
+                conn_stat = false;
+            }
+        }
+
+        public void disconnect()
+        {
+            send_data("disconnect");
+            socket.Close();
+            conn_stat = false;
+            StatusText("Disconnected by demand\r\n");
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            send_data("up");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            send_data("left");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            send_data("right");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            send_data("down");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            send_data("center");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (conn_stat == false)
+            {
+                connect();
+            }
+            else
+            {
+                disconnect();
+            }
+        }
+
+        private void Up2_Click(object sender, EventArgs e)
+        {
+            send_data("up2");
+        }
+
+        private void Up3_Click(object sender, EventArgs e)
+        {
+            send_data("up3");
+        }
+
+        private void Left2_Click(object sender, EventArgs e)
+        {
+            send_data("right2");
+        }
+
+        private void Left3_Click(object sender, EventArgs e)
+        {
+            send_data("right3");
+        }
+
+        private void Right2_Click(object sender, EventArgs e)
+        {
+            send_data("left2");
+        }
+
+        private void Right3_Click(object sender, EventArgs e)
+        {
+            send_data("left3");
+        }
+
+        private void Down2_Click(object sender, EventArgs e)
+        {
+            send_data("down2");
+        }
+
+        private void Down3_Click(object sender, EventArgs e)
+        {
+            send_data("down3");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
